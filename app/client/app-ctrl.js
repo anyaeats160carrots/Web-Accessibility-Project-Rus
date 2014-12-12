2b71062908a6e6f9e402333bc4a6890ffb6bb9e4
anyaeats160carrots <anna.a.kozlenko@gmail.com>
2014-12-12 02:31:27 -0700

    more source files

---
+++ app/client/app-ctrl.js
@@ -0,0 +1,8 @@
+function headerController($scope) {
+    $scope.tabs = [];
+    if ($scope.currentPage='home') {
+        $scope.tabs.add('Проверить');
+        $scope.tabs.add('Проверить');
+    }
+
+}
